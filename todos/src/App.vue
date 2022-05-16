b340540d069365ab678bb7cd339e5636560e8dcf
shandian <1638462769@qq.com>
2022-05-16 22:21:34 +0800

    完成项目相关资源准备

--- todos/src/App.vue
+++ todos/src/App.vue
@@ -6,6 +6,19 @@
 
 <script>
 export default {
-  name: 'App',
+  name: 'MyApp',
+  data () {
+    return {
+      // 定义任务列表的数据
+      todolist: [
+        { id: 1, task: '周一早上九点早起', done: false },
+        { id: 2, task: '周一晚上八点吃早餐', done: false },
+        { id: 3, task: '准备会议', done: true }
+      ]
+    }
+  }
 }
 </script>
+
+<style lang="less" scoped>
+</style>
